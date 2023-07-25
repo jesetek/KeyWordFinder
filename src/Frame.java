81ed5072a68c566a006b7a811bf731e8e51e007f
Andraz <jeseandraz@gmail.com>
2023-07-25 18:19:26 +0200

    Added KeyWord search with PDFBox lib + Modified select folder

--- src/Frame.java
+++ src/Frame.java
@@ -2,8 +2,32 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
 
 public class Frame {
+
+    private JTextField keywordTextField;
+
     public void initFrame() {
         JFrame jf = new JFrame();
         JPanel jp = new JPanel();
@@ -12,9 +36,12 @@ public class Frame {
         JLabel inputLabel = new JLabel("Izberite vrsto datoteke: ");
         jp.add(inputLabel);
 
-        String[] options = { "doc/docx", "pdf", "xls/xlsx" };
+        String[] options = { "pdf", "docx", "xlsx" };
         JComboBox<String> comboBox = new JComboBox<>(options);
         jp.add(comboBox);
+
+        keywordTextField = new JTextField(20);
+        jp.add(keywordTextField);
 
         JButton folderPickerButton = new JButton("Select Folder");
         jp.add(folderPickerButton);
@@ -27,6 +54,7 @@ public class Frame {
 
         /* FUNKCIJE */
 
+        // ActionListener for the folder picker button
         folderPickerButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -39,11 +67,64 @@ public class Frame {
                 // Check if a folder was selected
                 if (result == JFileChooser.APPROVE_OPTION) {
                     // Get the selected folder
-                    java.io.File selectedFolder = folderChooser.getSelectedFile();
-                    // Do something with the selected folder, such as displaying its path
-                    System.out.println("Selected Folder: " + selectedFolder.getAbsolutePath());
+                    File selectedFolder = folderChooser.getSelectedFile();
+
+                    // Get the selected file ending (e.g., pdf)
+                    String selectedEnding = comboBox.getSelectedItem().toString();
+
+                    // Get the keyword entered by the user
+                    String keyword = keywordTextField.getText().trim().toLowerCase();
+
+                    // Find documents with the selected file ending and containing the keyword
+                    List<File> pdfFiles = findFilesWithEndingAndKeyword(selectedFolder, selectedEnding, keyword);
+
+                    // Display the list of PDF files containing the keyword
+                    if (!pdfFiles.isEmpty()) {
+                        for (File file : pdfFiles) {
+                            System.out.println("PDF File with the keyword: " + file.getAbsolutePath());
+                        }
+                    } else {
+                        System.out.println("No PDF files found with the selected ending and containing the keyword.");
+                    }
                 }
             }
         });
     }
+
+    private List<File> findFilesWithEndingAndKeyword(File folder, String fileEnding, String keyword) {
+        List<File> result = new ArrayList<>();
+        if (folder.isDirectory()) {
+            try (Stream<Path> walk = Files.walk(folder.toPath())) {
+                List<String> files = walk.map(Path::toString)
+                        .filter(f -> f.toLowerCase().endsWith("." + fileEnding.toLowerCase()))
+                        .collect(Collectors.toList());
+
+                for (String filePath : files) {
+                    if (containsKeyword(filePath, keyword)) {
+                        result.add(new File(filePath));
+                    }
+                }
+            } catch (IOException e) {
+                // e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    private boolean containsKeyword(String filePath, String keyword) {
+        try {
+            File file = new File(filePath);
+            PDDocument doc = Loader.loadPDF(file);
+            PDFTextStripper findPhrase = new PDFTextStripper();
+            String text = findPhrase.getText(doc);
+            String PDF_content = text.toLowerCase();
+            boolean result = PDF_content.contains(keyword);
+            doc.close();
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 }
