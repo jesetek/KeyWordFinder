707f89af31ee4707d4aa3170d00022ca77321e8e
Andraz <jeseandraz@gmail.com>
2023-07-26 00:50:35 +0200

    Final changes and comments in code

--- src/App.java
+++ src/App.java
@@ -1,3 +1,11 @@
+/* Made by Andraž Ješe */
+/* July, 2023 */
+
+/* Version 1.0 SLO */
+/* Program omogoca PDF datotek, ki vsebujejo zeljeno kljucno besedo */
+/* Program allows us to search for PDF files with wanted keyword in it */
+/* Program je v slovenskem jeziku */
+
 public class App {
     public static void main(String[] args) throws Exception {
         Frame frame = new Frame();
--- src/Frame.java
+++ src/Frame.java
@@ -1,9 +1,11 @@
 import java.awt.*;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -19,46 +21,85 @@ import org.apache.pdfbox.text.PDFTextStripper;
 
 public class Frame {
 
-    private JFrame jf;
-    private JPanel jp;
-    private JTextField keywordTextField;
-    private JFileChooser  folderChooser;
-    private int result;
-    private JComboBox<String> comboBox;
-    private JCheckBox checkBox;
-    private JPanel resultsPanel;
-    private JScrollPane scrollPane;
+    public static JFrame jf;
+    public static JPanel jp;
+    public static JTextField keywordTextField;
+    public static JFileChooser folderChooser;
+    public static int result;
+    public static JComboBox<String> comboBox;
+    public static JCheckBox checkBox;
+    public static JPanel resultsPanel;
+    public static JScrollPane scrollPane;
+    public static JProgressBar progressBar;
+    public static JButton folderPickerButton;
+    public static JPanel panel;
+    public static java.util.List<File> pdfFiles;
+
+    private static File lastSelectedDir = null; // Store the last selected directory
 
     public void initFrame() {
         jf = new JFrame();
-        jp = new JPanel();
-        jp.setPreferredSize(new Dimension(400, 400));// changed it to preferredSize, Thanks!
+        jf.setTitle("KeyWord Finder");
 
-        JLabel inputLabel = new JLabel("Izberite vrsto datoteke: ");
-        jp.add(inputLabel);
+        BufferedImage icon;
+        try {
+            icon = ImageIO.read(new File("src/KeyWord.png"));   //icon of the app
+            jf.setIconImage(icon);
 
-        String[] options = { "pdf", "docx", "xlsx" };
+        } catch (IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+
+        jp = new JPanel();  //full panel
+        jp.setPreferredSize(new Dimension(500, 600));   // dimensions 500*600
+        panel = new JPanel();   //panel for static data (input, KW, button, CB)
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS)); // Set the layout to vertical BoxLayout
+
+        JLabel inputLabel = new JLabel("Izberite vrsto datoteke: ");    //input elements
+        String[] options = { "pdf" }; // , "docx", "xlsx" --> pride v novi verziji
         comboBox = new JComboBox<>(options);
-        jp.add(comboBox);
 
-        JLabel kwLabel = new JLabel("Vnesite ključno/e besedo/e: ");
-        jp.add(kwLabel);
+        JPanel inputPanel = new JPanel();   // input panel
+        inputPanel.add(inputLabel);
+        inputPanel.add(comboBox);
+        panel.add(inputPanel);
 
+        JLabel kwLabel = new JLabel("Vnesite ključno/e besedo/e: ");    //KW elements
         keywordTextField = new JTextField(20);
-        jp.add(keywordTextField);
 
-        JButton folderPickerButton = new JButton("Select Folder");
-        jp.add(folderPickerButton);
+        JPanel keywordPanel = new JPanel(); //KW panel
+        keywordPanel.add(kwLabel);
+        keywordPanel.add(keywordTextField);
+        panel.add(keywordPanel);
 
-        checkBox = new JCheckBox();
-        jp.add(checkBox);
+        folderPickerButton = new JButton("Izberite mapo");  //button element + panel
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        buttonPanel.add(folderPickerButton);
+        panel.add(buttonPanel);
 
+        checkBox = new JCheckBox(); //CB elements
         JLabel cbLabel = new JLabel("Celotna pot do datoteke");
-        jp.add(cbLabel);
 
-        resultsPanel = new JPanel();
+        JPanel checkboxPanel = new JPanel();    //CB panel
+        checkboxPanel.add(checkBox);
+        checkboxPanel.add(cbLabel);
+        panel.add(checkboxPanel);
+
+        progressBar = new JProgressBar(0, 100); //progress bar
+        progressBar.setPreferredSize(new Dimension(200, 20));
+        progressBar.setStringPainted(true); // Display percentage as text
+        progressBar.setVisible(false);
+        panel.add(progressBar);
+
+        jp.add(panel);
+
+        resultsPanel = new JPanel();    //panel for dynamic data (results)
         resultsPanel.setLayout(new BoxLayout(resultsPanel, BoxLayout.Y_AXIS));
         scrollPane = new JScrollPane(resultsPanel);
+        scrollPane.getVerticalScrollBar().setUnitIncrement(16);
+        scrollPane.getHorizontalScrollBar().setUnitIncrement(16);
         jp.add(scrollPane);
 
         jf.getContentPane().add(jp);// adding to content pane will work here. Please read the comment bellow.
@@ -67,113 +108,221 @@ public class Frame {
                                                            // closed
         jf.setVisible(true); // Add this line to make the frame visible
 
-        /* FUNKCIJE */
+        /* FUNCTIONS */
 
-        // ActionListener for the folder picker button
+        //Function which searches for files with keyword in it
         folderPickerButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                
+                //if input is empty, we dont search
                 if (keywordTextField.getText().equals("")) {
                     JOptionPane.showMessageDialog(null, "Polje ključne besede je prazno!", "Alert",
                             JOptionPane.WARNING_MESSAGE);
                 } else {
 
                     folderChooser = new JFileChooser();
+
+                    // Set the current directory to the last selected directory
+                    if (lastSelectedDir != null) {
+                        folderChooser.setCurrentDirectory(lastSelectedDir);
+                    }
                     // Show the folder chooser dialog
                     folderChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
                     result = folderChooser.showOpenDialog(jf);
 
-                    folderPick(folderChooser, result);
-                    
+                    new Thread(new FolderPick()).start();   //we have to start a thread so we can update progress bar
+
                 }
             }
         });
 
+        //CB function which changes length of data to full pat or only shortened path
+        // function is a part of the main Thread so everything is described there
         checkBox.addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
+                int hgtScroll = 0;
+
                 if (e.getStateChange() == ItemEvent.SELECTED) {
-                    if (keywordTextField.getText().equals("")) {
-
-                    } else {
-                        folderPick(folderChooser, result);
+                    // Clear previous results
+                    if (resultsPanel != null) {
+                        resultsPanel.removeAll();
                     }
+                    if (!pdfFiles.isEmpty()) {
+                        for (File file : pdfFiles) {
+                            addResultToPanel(file.getAbsolutePath());
+                        }
+                    } else {
+                        JPanel resultPanel = new JPanel();
+                        resultPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+                        // Create a label to display the file path
+                        JLabel label = new JLabel("Brez zadetkov");
+                        resultPanel.add(label);
+
+                        resultsPanel.add(resultPanel);
+                    }
+                    hgtScroll = (pdfFiles.size() + 1) * 32;
+                    if (hgtScroll > 580 - panel.getPreferredSize().height) {
+                        hgtScroll = 580 - panel.getPreferredSize().height;
+                    }
+                    scrollPane.setPreferredSize(new Dimension(480, hgtScroll));
+                    // Refresh the UI
+                    resultsPanel.revalidate();
+                    resultsPanel.repaint();
+                    scrollPane.revalidate();
+                    scrollPane.repaint();
+                    jp.revalidate();
+                    jp.repaint();
+                    jf.revalidate();
+                    jf.repaint();
+                    progressBar.setVisible(false);
+                    folderPickerButton.setEnabled(true);
+                    checkBox.setEnabled(true);
+
                 } else if (e.getStateChange() == ItemEvent.DESELECTED) {
-                    if (keywordTextField.getText().equals("")) {
-
-                    } else {
-                        folderPick(folderChooser, result);
+                    // Clear previous results
+                    if (resultsPanel != null) {
+                        resultsPanel.removeAll();
                     }
+                    if (!pdfFiles.isEmpty()) {
+                        for (File file : pdfFiles) {
+                            addResultToPanel(file.getAbsolutePath());
+                        }
+                    } else {
+                        JPanel resultPanel = new JPanel();
+                        resultPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+                        // Create a label to display the file path
+                        JLabel label = new JLabel("Brez zadetkov");
+                        resultPanel.add(label);
+
+                        resultsPanel.add(resultPanel);
+                    }
+                    hgtScroll = (pdfFiles.size() + 1) * 32;
+                    if (hgtScroll > 600 - panel.getPreferredSize().height) {
+                        hgtScroll = 600 - panel.getPreferredSize().height;
+                    }
+                    scrollPane.setPreferredSize(new Dimension(480, hgtScroll));
+                    // Refresh the UI
+                    resultsPanel.revalidate();
+                    resultsPanel.repaint();
+                    scrollPane.revalidate();
+                    scrollPane.repaint();
+                    jp.revalidate();
+                    jp.repaint();
+                    jf.revalidate();
+                    jf.repaint();
+                    progressBar.setVisible(false);
+                    folderPickerButton.setEnabled(true);
+                    checkBox.setEnabled(true);
+
                 }
             }
         });
+        
     }
 
-    private void folderPick(JFileChooser folderChooser, int result){
+    //MAIN thread/function/class
+    public static class FolderPick implements Runnable {
 
-                    // Check if a folder was selected
-                    if (result == JFileChooser.APPROVE_OPTION) {
-                        // Get the selected folder
-                        File selectedFolder = folderChooser.getSelectedFile();
+        @Override
+        public void run() {
 
-                        // Get the selected file ending (e.g., pdf)
-                        String selectedEnding = comboBox.getSelectedItem().toString();
+            int hgtScroll = 0;
 
-                        // Get the keyword entered by the user
-                        String keyword = keywordTextField.getText().trim().toLowerCase();
+            // Check if a folder was selected
+            if (result == JFileChooser.APPROVE_OPTION) {
 
-                        // Find documents with the selected file ending and containing the keyword
-                        java.util.List<File> pdfFiles = findFilesWithEndingAndKeyword(selectedFolder, selectedEnding,
-                                keyword);
+                progressBar.setVisible(true);
+                folderPickerButton.setEnabled(false);
+                checkBox.setEnabled(false);
 
-                        // Clear previous results
-                        if (resultsPanel != null) {
-                            resultsPanel.removeAll();
-                        }
+                // Get the selected folder
+                File selectedFolder = folderChooser.getSelectedFile();
+                lastSelectedDir = selectedFolder;
 
-                        // Display the list of PDF files containing the keyword
-                        if (!pdfFiles.isEmpty()) {
-                            for (File file : pdfFiles) {
-                                if (checkBox.isSelected()) {
-                                    addResultToPanel(file.getAbsolutePath());
-                                } else {
+                // Get the selected file ending (e.g., pdf)
+                String selectedEnding = comboBox.getSelectedItem().toString();
 
-                                    String[] pathParts = file.getAbsolutePath().split("\\\\");
-                                    if (pathParts.length > 2) {
-                                        addResultToPanel(pathParts[0] + "/" + pathParts[1] + "/.../"
-                                                + pathParts[pathParts.length - 1]);
-                                    } else {
-                                        addResultToPanel(pathParts[0] + "/" + pathParts[1]);
-                                    }
-                                }
-                            }
-                        } else {
-                            addResultToPanel("Brez zadetkov");
-                        }
+                // Get the keyword entered by the user
+                String keyword = keywordTextField.getText().trim().toLowerCase();
 
-                        scrollPane.setPreferredSize(new Dimension(400, scrollPane.getPreferredSize().height + 50));
-                        // Refresh the UI
-                        resultsPanel.revalidate();
-                        resultsPanel.repaint();
-                        scrollPane.revalidate();
-                        scrollPane.repaint();
-                        jp.revalidate();
-                        jp.repaint();
-                        jf.revalidate();
-                        jf.repaint();
+                // Find documents with the selected file ending and containing the keyword
+                pdfFiles = findFilesWithEndingAndKeyword(selectedFolder, selectedEnding,
+                        keyword);
+
+                // Clear previous results
+                if (resultsPanel != null) {
+                    resultsPanel.removeAll();
+                }
+
+                // Display the list of PDF files containing the keyword
+                if (!pdfFiles.isEmpty()) {
+                    for (File file : pdfFiles) {
+                        addResultToPanel(file.getAbsolutePath());
                     }
+                } else {
+                    JPanel resultPanel = new JPanel();
+                    resultPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+                    // Create a label to display the file path
+                    JLabel label = new JLabel("Brez zadetkov");
+                    resultPanel.add(label);
+
+                    resultsPanel.add(resultPanel);
+                }
+
+                //setting height of the results panel
+                hgtScroll = (pdfFiles.size() + 1) * 32;
+                if (hgtScroll > 600 - panel.getPreferredSize().height) {
+                    hgtScroll = 600 - panel.getPreferredSize().height;
+                }
+                scrollPane.setPreferredSize(new Dimension(480, hgtScroll));
+                // Refresh the UI
+                resultsPanel.revalidate();
+                resultsPanel.repaint();
+                scrollPane.revalidate();
+                scrollPane.repaint();
+                jp.revalidate();
+                jp.repaint();
+                jf.revalidate();
+                jf.repaint();
+                progressBar.setVisible(false);
+                folderPickerButton.setEnabled(true);
+                checkBox.setEnabled(true);
+            }
+        }
     }
 
-    private void addResultToPanel(String filePath) {
+    private static void addResultToPanel(String filePath) {
+
+        String txt = "";
+
         JPanel resultPanel = new JPanel();
         resultPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 
+        //checking for checkbox state and fiduring out in which way to display data
+        if (checkBox.isSelected()) {
+            txt = filePath;
+        } else {
+
+            String[] pathParts = filePath.split("\\\\");
+            if (pathParts.length > 2) {
+                txt = pathParts[0] + "\\" + pathParts[1] + "\\...\\"
+                        + pathParts[pathParts.length - 1];
+            } else {
+                txt = filePath;
+            }
+        }
+
         // Create a label to display the file path
-        JLabel label = new JLabel(filePath);
+        JLabel label = new JLabel(txt);
         resultPanel.add(label);
 
         // Create a button for opening the file
-        JButton openButton = new JButton("Open");
+        JButton openButton = new JButton("Odpri");
         openButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -191,18 +340,29 @@ public class Frame {
         resultsPanel.add(resultPanel);
     }
 
-    private List<File> findFilesWithEndingAndKeyword(File folder, String fileEnding, String keyword) {
+    //function that finds matches
+    private static List<File> findFilesWithEndingAndKeyword(File folder, String fileEnding, String keyword) {
         List<File> result = new ArrayList<>();
+
+        int filesProcessed = 0;
+
         if (folder.isDirectory()) {
             try (Stream<Path> walk = Files.walk(folder.toPath())) {
                 List<String> files = walk.map(Path::toString)
                         .filter(f -> f.toLowerCase().endsWith("." + fileEnding.toLowerCase()))
                         .collect(Collectors.toList());
 
+                int size = files.size();
+
                 for (String filePath : files) {
                     if (containsKeyword(filePath, keyword)) {
                         result.add(new File(filePath));
                     }
+                    //updating progress bar
+                    filesProcessed++;
+                    int progressPercentage = (int) ((double) filesProcessed / size * 100);
+                    //System.out.println(progressPercentage);
+                    progressBar.setValue(progressPercentage);
                 }
             } catch (IOException e) {
                 // e.printStackTrace();
@@ -211,7 +371,7 @@ public class Frame {
         return result;
     }
 
-    private boolean containsKeyword(String filePath, String keyword) {
+    private static boolean containsKeyword(String filePath, String keyword) {
         try {
             File file = new File(filePath);
             PDDocument doc = Loader.loadPDF(file);
